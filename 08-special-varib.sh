e154e5c0dc3112def7ea4b50c3ad7e1d9537b6c6
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-25 14:56:46 +0530

    shell script

--- 08-special-varib.sh
+++ 08-special-varib.sh
@@ -5,3 +5,5 @@ echo "Number of variables passed: $#"
 echo "script Name: $0"
 echo "current working directory:$PWD"
 echo "Home directory of current user: $HOME"
+echo "which user is running this script:$user"
+
