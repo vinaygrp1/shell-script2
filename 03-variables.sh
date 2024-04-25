f604a2decea45bd9051b1fe75750aac12b366226
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-25 12:33:41 +0530

    shell script

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of varible
+PERSON1=SACHIN #NO SPACE BETWEEN THEM
+PERSON2=RAHUL
+
+#REFERRING VARIBLE
+echo "$PERSON1:: hELLO $PERSON2, HOW ARE YOU?"
+echo "$PERSON2:: Hi $PERSON2, I am fine. how are you?"
+echo "$PERSON1:: I am fine too. $PERSON2, how is your work?"
+echo "$PERSON2:: hELLO, HOW ARE YOU?"
