95dc20a23a86f7e2a41821093e49284eb95ebc11
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-25 12:25:14 +0530

    shell script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: Hello suresh, how are you?"
+echo "suresh:: Hi ramesh, I am fine. How r You?"
+echo "Ramesh:: I am fine too. how is your work?"
+echo "suresh:: not bad. I am thinking to upgrade to devops"
