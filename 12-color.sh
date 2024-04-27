86c5e4c5ec93da346f83fa9b759389153a6eb108
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-27 12:11:15 +0530

    colors

--- 12-color.sh
+++ 12-color.sh
@@ -35,5 +35,6 @@ VALIDATE $? "Installing Mysql"
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
 
-dnf install dockerr -y &>>$LOGFILE
+dnf install docker -y &>>$LOGFILE
 VALIDATE $? "Installing docker"
+ 
