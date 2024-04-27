3f01a692e7bc40ffb697758b8ddfaa162b246dd4
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-27 13:06:21 +0530

    packages

---
+++ 14 install-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+
+then
+    echo "please run this script with root access."
+    exit 1 #manually exit if error comes
+else
+    echo "you are super user."
+fi
+
+echo "All packages: $@"
