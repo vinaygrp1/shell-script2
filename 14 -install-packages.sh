c25f489f7dc629b425d7741e6a0e7d9bd9f2c8b4
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-27 15:23:46 +0530

    packages

--- 14 -install-packages.sh
+++ 14 -install-packages.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...FAILURE"
+        exit 1
+    else
+        echo -e "$2...SUCCESS"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 
@@ -15,4 +28,11 @@ for i in $@
 do
     echo "packages to install:$i"
     dnf list installed $i
+    if [ $? -eq 0]
+    then
+        echo "$! already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
+    fi
+    
 done
