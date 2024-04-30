277037477b9a0f9ea4f95066f8ae5b53860b1378
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-30 14:40:39 +0530

    shell-script2

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops from current script"
 echo "Before calling other script, course:$COURSE"
 echo "Process ID of Current Shell Script:$$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "After calling other script, course:$COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+COURSE="Devops from other script"
+echo "variable value from other script:$COURSE"
+echo "Process ID of other script:$$"
