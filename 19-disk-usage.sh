8b105131a75f9a472434b5be1d54f108dafd6030
vinaygrp1 <vinaygrp1@gmail.com>
2024-05-04 23:22:07 +0530

    shell-script2

---
+++ 19-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_THRESHOLD=$(df -hT | grep xfs)
+DISK_TRESHOLD=6
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    #USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
+    fi
+
+done <<< $DISK_USAGE 
